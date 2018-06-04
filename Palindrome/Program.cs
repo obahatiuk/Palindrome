9898f00addd14c16450403e69e4a4f64f0ceb48a
obahatiuk <36489946+obahatiuk@users.noreply.github.com>
2018-06-04 17:15:59 -0400

    first commit

---
+++ Palindrome/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        public static bool IsPalindrome(string word)
+        {
+            bool isPalindrome = true;
+            int halfLength = (int)Math.Floor(word.Length / 2.0);
+            word = word.ToLower();
+
+            for (int i = 0; i < halfLength; i++)
+            {
+
+                char first = word[i];
+                char second = word[word.Length - 1 - i];
+
+                if (word[i] != word[word.Length - 1 - i])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+
+
+            }
+
+            return isPalindrome;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(IsPalindrome("Deleveled"));
+            Console.ReadKey();
+
+        }
+
+    }
+}
